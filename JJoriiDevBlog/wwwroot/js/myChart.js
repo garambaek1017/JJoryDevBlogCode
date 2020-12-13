a4f40ea28c47334caf36bb183ce2a8ecf3aa4370
MyBoySummer <developerbgr@gmail.com>
2020-12-13 16:00:04 +0900

    git.blog - css 정리

---
+++ JJoriiDevBlog/wwwroot/js/myChart.js
@@ -0,0 +1,17 @@
+﻿window.MyAppJsFunctions = {
+    drawChart: function () {
+        var ctx = document.getElementById("myPieChart");
+
+        const myPieChart = new Chart(ctx, {
+            type: 'pie',
+
+            data: {
+                labels: ["Blue", "Red", "Yellow", "Green"],
+                datasets: [{
+                    data: [12.21, 15.58, 11.25, 8.32],
+                    backgroundColor: ['#007bff', '#dc3545', '#ffc107', '#28a745'],
+                }],
+            },
+        });
+    },
+};
